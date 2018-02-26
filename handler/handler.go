c3d88b31d1c881163601900d47cc24563abf4609
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-02-26 10:49:52 +0300

    some little fixes

--- handler/handler.go
+++ handler/handler.go
@@ -1,44 +1,33 @@
 package handler
 
 import (
-	"log"
-	"net"
-
 	"fmt"
-
-	"os"
+	"net"
 
 	"github.com/Sharykhin/gl-mail-grpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
-var serverSource string
-
 var (
 	crt = "server.crt"
 	key = "server.key"
 )
 
-func init() {
-	serverSource = os.Getenv("SERVER_SOURCE")
-}
-
-func ListenAndServe() {
-	fmt.Printf("Start listening on %s \n", serverSource)
+func ListenAndServe(serverSource string) error {
 	lis, err := net.Listen("tcp", serverSource)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	// Create the TLS credentials
 	cred, err := credentials.NewServerTLSFromFile(crt, key)
 	if err != nil {
-		log.Fatalf("could not load TLS keys: %v", err)
+		return fmt.Errorf("could not load TLS keys: %v", err)
 	}
 
 	// Creates a new gRPC server
 	s := grpc.NewServer(grpc.Creds(cred))
 	api.RegisterFailMailServer(s, &server{})
-	log.Fatal(s.Serve(lis))
+	return s.Serve(lis)
 }
--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/Sharykhin/gl-mail-grpc-server/handler"
+	"log"
+	"fmt"
+	"os"
 )
 
 func main() {
-	handler.ListenAndServe()
+	serverSource := os.Getenv("SERVER_SOURCE")
+	fmt.Printf("Start listening on %s \n", serverSource)
+	log.Fatal(handler.ListenAndServe(serverSource))
 }
