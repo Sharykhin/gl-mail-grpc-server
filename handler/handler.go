0ea2a2c59052a33b8d45eca6857a1a6856d5dd4b
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-03-02 11:14:03 +0300

    works with insecure mode

--- handler/handler.go
+++ handler/handler.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"net"
 
+	"os"
+
+	"log"
+
 	"github.com/Sharykhin/gl-mail-grpc"
 	"github.com/Sharykhin/gl-mail-grpc-server/database"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
-	crt = "server.crt"
-	key = "server.key"
+	crt string
+	key string
 )
 
 // ListenAndServe creates grps server and start listening income connections
@@ -22,15 +27,25 @@ func ListenAndServe(serverSource string) error {
 	}
 
 	// Create the TLS credentials
-	//cred, err := credentials.NewServerTLSFromFile(crt, key)
-	//if err != nil {
-	//	return fmt.Errorf("could not load TLS keys: %v", err)
-	//}
+	cred, err := credentials.NewServerTLSFromFile(crt, key)
+	if err != nil {
+		return fmt.Errorf("could not load TLS keys: %v", err)
+	}
 
 	// Creates a new gRPC server
-	//s := grpc.NewServer(grpc.Creds(cred))
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.Creds(cred))
 	storage := database.Storage
 	api.RegisterFailMailServer(s, &server{storage: storage})
 	return s.Serve(lis)
+}
+
+func init() {
+	crt = os.Getenv("KEY_SERVER_CRT")
+	if crt == "" {
+		log.Fatal("Env variable KEY_SERVER_CRT is not specified")
+	}
+	key = os.Getenv("KEY_SERVER_KEY")
+	if key == "" {
+		log.Fatal("Env variable KEY_SERVER_KEY is not specified")
+	}
 }
